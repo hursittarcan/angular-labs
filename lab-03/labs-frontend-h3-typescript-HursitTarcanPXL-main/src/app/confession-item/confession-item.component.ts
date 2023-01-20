2d8e099bec06c1fd3a76dcb9910531796e87bd26
HursitTarcan <HursitTarcan@gmail.com>
2023-01-20 21:36:43 +0100

    lab 3 - 75% done
    exams, exams, exames :)

--- lab-03/labs-frontend-h3-typescript-HursitTarcanPXL-main/src/app/confession-item/confession-item.component.ts
+++ lab-03/labs-frontend-h3-typescript-HursitTarcanPXL-main/src/app/confession-item/confession-item.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+class Confession {
+  body: string;
+  subject: string;
+  author: string;
+
+  constructor(body: string, subject: string, author: string) {
+    this.body = body;
+    this.subject = subject;
+    this.author = author;
+  }
+}
+
 @Component({
   selector: 'app-confession-item',
   templateUrl: './confession-item.component.html',
   styleUrls: ['./confession-item.component.css']
 })
 export class ConfessionItemComponent implements OnInit {
+  confession: Confession | undefined;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.confession = new Confession("onze tweede confession", "PXL-Digital","Dries");
   }
 
 }

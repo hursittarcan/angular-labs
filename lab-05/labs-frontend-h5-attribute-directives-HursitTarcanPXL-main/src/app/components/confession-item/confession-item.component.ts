b0e1a3fe9b720b7e29f831e2dbea004dde9daf60
HursitTarcan <HursitTarcan@gmail.com>
2023-01-21 16:19:56 +0100

    add department logo to confession

--- lab-05/labs-frontend-h5-attribute-directives-HursitTarcanPXL-main/src/app/components/confession-item/confession-item.component.ts
+++ lab-05/labs-frontend-h5-attribute-directives-HursitTarcanPXL-main/src/app/components/confession-item/confession-item.component.ts
@@ -20,4 +20,25 @@ export class ConfessionItemComponent implements OnInit {
   dislike(): void {
     this.confession.dislikes++;
   }
+
+  getDepartmentUrl(department: string): string {
+    department = department.toLowerCase();
+    switch(department) {
+      case 'pxl-digital': {
+        return 'assets/pxl-digital.png'
+      }
+      case 'pxl-mad': {
+        return 'assets/pxl-mad.png'
+      }
+      case 'pxl-business': {
+        return 'assets/pxl-business.png'
+      }
+      case 'pxl-education': {
+        return 'assets/pxl-education.png'
+      }
+      default: {
+        return 'assets/hogeschoolpxl.png'
+      }
+    }
+  }
 }

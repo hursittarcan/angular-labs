1a6f4a0f66aaf39687d53c62add58526d348b616
HursitTarcan <HursitTarcan@gmail.com>
2023-01-21 15:24:51 +0100

    add new logo component

--- exercises/exercise-01/chapter3-oefening1/chapter3-oefening1/src/app/app.module.ts
+++ exercises/exercise-01/chapter3-oefening1/chapter3-oefening1/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LogoComponentComponent } from './components/logo-component/logo-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogoComponentComponent
   ],
   imports: [
     BrowserModule

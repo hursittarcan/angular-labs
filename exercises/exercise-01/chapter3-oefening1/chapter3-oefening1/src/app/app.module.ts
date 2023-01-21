b5d28b3c9bb4cc9b49d87791558add8752d77853
HursitTarcan <HursitTarcan@gmail.com>
2023-01-21 15:24:06 +0100

    add extra exercise - chapter 3

--- exercises/exercise-01/chapter3-oefening1/chapter3-oefening1/src/app/app.module.ts
+++ exercises/exercise-01/chapter3-oefening1/chapter3-oefening1/src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { LogoComponentComponent } from './components/logo-component/logo-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LogoComponentComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
